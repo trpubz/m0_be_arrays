5ec6c6ff9f1719db5a368ac9ed45fe1380ae2185
Taylor Pubins <tpubz@icloud.com>
2023-08-01 17:02:58 -0700

    [arrays.rb] Add an example of different types of arrays.

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+
+pubins_family = ["Amanda", "Robby", "David", "Taylor"]
+my_favorite_nums = [3, 16, 26, 42]
+my_favorite_battingaverages = [0.400, 0.200, 0.300, 0.316]
+three_truths_and_alie = [true, false, true, true]
