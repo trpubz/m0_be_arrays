46667f94968a5cfa4ab103295aea86c2bc2fb5b9
Taylor Pubins <tpubz@icloud.com>
2023-08-01 17:08:21 -0700

    Refactor arrays.rb: modify array manipulation methods

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,19 @@ pubins_family = ["Amanda", "Robby", "David", "Taylor"]
 my_favorite_nums = [3, 16, 26, 42]
 my_favorite_battingaverages = [0.400, 0.200, 0.300, 0.316]
 three_truths_and_alie = [true, false, true, true]
+
+# pop the last element off the list leaving only the first 3 elements
+my_favorite_battingaverages.pop()
+p my_favorite_battingaverages
+
+# push a new element to the back of the list
+pubins_family.push("Sebass")
+p pubins_family
+
+# shift the first element off the list leaving only the last 3 elements
+my_favorite_nums.shift()
+p my_favorite_nums
+
+# unshift a new element to the front of the list
+three_truths_and_alie.unshift(false)
+p three_truths_and_alie
