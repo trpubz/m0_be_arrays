d351351a9fb67771222b0cab0f149b5b7b2e7a67
Taylor Pubins <tpubz@icloud.com>
2023-08-01 17:21:58 -0700

    Show index accessing

--- arrays.rb
+++ arrays.rb
@@ -19,3 +19,19 @@ p my_favorite_nums
 # unshift a new element to the front of the list
 three_truths_and_alie.unshift(false)
 p three_truths_and_alie
+
+# first element of the list
+p my_favorite_battingaverages[0] == 0.400
+
+# expect IndexError if accessing an index that doesn't exist when fetching
+begin
+  raise IndexError
+    p my_favorite_battingaverages.fetch(100)
+  rescue IndexError => e
+    p e.message
+end
+
+# expect nil if accessing an index that doesn't exist
+p my_favorite_battingaverages[100]
+
+
